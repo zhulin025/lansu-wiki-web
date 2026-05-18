4df33e2e1c0fc49ac36fb5088ef299c347f6b746
zhulin025 <zhulin025@users.noreply.github.com>
2026-05-18 17:56:21 +0800

    build: restrict cloudflare dev initializer to development mode

--- next.config.ts
+++ next.config.ts
@@ -14,4 +14,6 @@ const nextConfig: NextConfig = {
 export default nextConfig;
 
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-initOpenNextCloudflareForDev();
+if (process.env.NODE_ENV === "development") {
+  initOpenNextCloudflareForDev();
+}
